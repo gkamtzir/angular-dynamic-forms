e6d58c04e08f6dfeaae1c2e53f87801ee970c207
George Kamtziridis <georgekam96@gmail.com>
2020-01-11 16:32:51 +0200

    Created and integrated an input-radio.

--- projects/angular-dynamic-forms-lib/src/lib/angular-dynamic-forms-lib.module.ts
+++ projects/angular-dynamic-forms-lib/src/lib/angular-dynamic-forms-lib.module.ts
@@ -15,13 +15,15 @@ import { AngularDynamicFormsLibComponent } from './angular-dynamic-forms-lib.com
 import { InputBoxComponent } from './components/input-box/input-box.component';
 import { InputSelectComponent } from './components/input-select/input-select.component';
 import { InputCheckboxComponent } from './components/input-checkbox/input-checkbox.component';
+import { InputRadioComponent } from './components/input-radio/input-radio.component';
 
 @NgModule({
   declarations: [
     AngularDynamicFormsLibComponent,
     InputBoxComponent,
     InputSelectComponent,
-    InputCheckboxComponent
+    InputCheckboxComponent,
+    InputRadioComponent
   ],
   imports: [
     ReactiveFormsModule,
