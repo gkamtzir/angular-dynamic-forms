8feb7b6517592d75a349c71b87cde005fd037cf5
George Kamtziridis <georgekam96@gmail.com>
2020-01-11 13:22:26 +0200

    Integrated the text-input component.

--- projects/angular-dynamic-forms-lib/src/lib/components/text-input/text-input.component.ts
+++ projects/angular-dynamic-forms-lib/src/lib/components/text-input/text-input.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+// Interfaces
+import { IConfiguration } from '../../interfaces/configuration.interface';
 
 @Component({
-  selector: 'adf-text-input',
+  selector: 'text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.css']
 })
 export class TextInputComponent implements OnInit {
 
+  // Inputs
+  @Input() form: FormGroup;
+  @Input() input: IConfiguration;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.input);
   }
 
 }
